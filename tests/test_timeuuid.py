dde0d3aec30ea4475a6585902db3191a8475d615
Usman Masood <usmanm@locu.com>
2014-07-12 22:57:07 +0000

    Add reverse sort ability

--- tests/test_timeuuid.py
+++ tests/test_timeuuid.py
@@ -9,32 +9,47 @@ from timeuuid import TimeUUID
 def py_cmp(tu1, tu2):
   return cmp((tu1.time, tu1.bytes), (tu2.time, tu2.bytes))
 
-def get_uuids(n=1, version=1):
-  if version == 1:
-    u = lambda: uuid.uuid1(random.randint(0, 2**48 - 1))
-  elif version == 4:
-    u = lambda: uuid.uuid4()
-  else:
-    raise ValueError
+def get_str_uuids(n):
   for _ in xrange(n):
-    yield str(u())
+    yield str(uuid.uuid4())
 
 class TestTimeUUID(unittest.TestCase):
   def test_time(self):
-    for _id in get_uuids(n=5000):
+    for _id in get_str_uuids(5000):
       uu = UUID(_id)
       tuu = TimeUUID(_id)
       self.assertEqual(uu.time, tuu.time)
 
   def test_bytes(self):
-    for _id in get_uuids(n=5000, version=4):
+    for _id in get_str_uuids(5000):
       uu = UUID(_id)
       tuu = TimeUUID(_id)
       self.assertEqual(uu.bytes, tuu.bytes)
 
   def test_cmp(self):
-    uuids = map(lambda s: TimeUUID(s), get_uuids(n=5000))
+    uuids = map(lambda s: TimeUUID(s), get_str_uuids(5000))
     random.shuffle(uuids)
     for _ in xrange(5000):
       a, b = random.choice(uuids), random.choice(uuids)
       self.assertEqual(cmp(a, b), py_cmp(a, b))
+
+    for _ in xrange(100):
+      i = random.randint(1, 4999)
+      self.assertTrue(uuids[i] == uuids[i])
+      self.assertTrue(uuids[i] >= uuids[i])
+      self.assertTrue(uuids[i] <= uuids[i])
+      self.assertFalse(uuids[i] != uuids[i])
+      self.assertFalse(uuids[i - 1] == uuids[i])
+      self.assertTrue(uuids[i - 1] != uuids[i])
+
+  def test_reverse(self):
+    uuid_strs = list(get_str_uuids(100))
+    uuids = sorted(map(lambda s: TimeUUID(s), uuid_strs))
+    reverse_uuids = sorted(map(lambda s: TimeUUID(s, reverse=True), uuid_strs))
+    self.assertEqual(uuids, reverse_uuids[::-1])
+
+    for uu, ruu in zip(uuids, reverse_uuids[::-1]):
+      self.assertTrue(uu == ruu)
+      self.assertTrue(uu <= ruu)
+      self.assertTrue(uu >= ruu)
+      self.assertFalse(uu != ruu)
--- timeuuid/timeuuid.pyx
+++ timeuuid/timeuuid.pyx
@@ -3,18 +3,21 @@
 #
 # cython: boundscheck=False, wraparound=False, embedsignature=True
 
+from cpython cimport bool
 from cpython.object cimport Py_EQ, Py_GE, Py_GT, Py_LE, Py_LT, Py_NE
 
+from uuid cimport int32_t
 from uuid cimport memcmp
+from uuid cimport uint64_t
 from uuid cimport uuid_parse
 from uuid cimport uuid_t
-from uuid cimport uint64_t
 
 cdef class TimeUUID:
   cdef uuid_t _bytes
   cdef readonly uint64_t time
+  cdef public bint reverse
   
-  def __cinit__(TimeUUID self, str uuid_pystr):
+  def __cinit__(TimeUUID self, str uuid_pystr, bool reverse=False):
     uuid_pyutf8 = uuid_pystr.encode('utf-8')
     cdef char *uuid_cstr = uuid_pyutf8
     uuid_parse(uuid_cstr, self._bytes)
@@ -31,26 +34,28 @@ cdef class TimeUUID:
     tmp = (self._bytes[6] << 8) | self._bytes[7]
     self.time = self.time | ((tmp & 0xfff) << 48)
 
+    self.reverse = reverse
+
   def __richcmp__(TimeUUID self, TimeUUID other, int op):
-    cdef int bytes_cmp = memcmp(&self._bytes[0], &other._bytes[0], 16)
+    cdef bint result = 0
+    cdef int32_t bytes_cmp = memcmp(&self._bytes[0], &other._bytes[0], 16)
 
     if op == Py_EQ:
       return bytes_cmp == 0
     if op == Py_NE:
       return bytes_cmp != 0
+    
+    if bytes_cmp == 0 and (op == Py_GE or op == Py_LE):
+      return True
 
-    if op == Py_GE:
-      return self.time > other.time or (self.time == other.time and
-                                        bytes_cmp >= 0)
-    if op == Py_GT:
-      return self.time > other.time or (self.time == other.time and
-                                        bytes_cmp > 0)
-    if op == Py_LE:
-      return self.time < other.time or (self.time == other.time and
-                                        bytes_cmp <= 0)
-    if op == Py_LT:
-      return self.time < other.time or (self.time == other.time and
-                                        bytes_cmp < 0)
+    if op == Py_GE or op == Py_GT:
+      result = self.time > other.time or (self.time == other.time and
+                                          bytes_cmp > 0)
+    elif op == Py_LE or op == Py_LT:
+      result = self.time < other.time or (self.time == other.time and
+                                          bytes_cmp < 0)
+    
+    return result ^ self.reverse
 
   property bytes:
     def __get__(self):
